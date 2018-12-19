0cfa28741effaa87c1d62bd624fb6b4c49076d17
czx <czx@DESKTOP-R69DP2P>
2018-12-19 16:44:01 +0800

    Initial commit

---
+++ src/main/java/xin/zexing/demo/DemoApplication.java
@@ -0,0 +1,17 @@
+package xin.zexing.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author czx
+ */
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
+
---
+++ src/main/java/xin/zexing/demo/web/ServerController.java
@@ -0,0 +1,11 @@
+package xin.zexing.demo.web;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by czx on 2018/12/19.
+ */
+@Controller
+public class ServerController {
+
+}
